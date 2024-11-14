a57bf8a1c3842c593258b2013488f57cfdfebe80
Cliff Wu <z800201002005@gmail.com>
2024-11-14 19:38:33 +0900

    Add tkinter sample program

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
+from tkinter import *
+from tkinter import ttk
+
+
 def main():
-    print("Hello, world!")
+    root = Tk()
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+    root.mainloop()
 
 
 if __name__ == "__main__":
